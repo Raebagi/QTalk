fcc2d32c22bff7e3ee54aa830a210d0c82352151
Hichanmin <pcm4106@naver.com>
2024-09-05 14:47:41 +0900

    First Commit

---
+++ src/main/java/org/example/qtalk/QtalkApplication.java
@@ -0,0 +1,13 @@
+package org.example.qtalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QtalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QtalkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/qtalk/QtalkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.qtalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QtalkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
