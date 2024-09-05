4b0a8e2d251249bc0a8635e98e0366443db139be
leeyejun <leeyejun1103@naver.com>
2024-09-05 16:27:27 +0900

    test1234

--- src/main/java/org/example/qtalk/QtalkApplication.java
+++ src/main/java/org/example/qtalk/QtalkApplication.java
@@ -2,14 +2,14 @@ package org.example.qtalk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.json.GsonBuilderUtils;
 
 @SpringBootApplication
 public class QtalkApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(QtalkApplication.class, args);
-    }
-
+        System.out.println("hello");
     //test
 
 }
